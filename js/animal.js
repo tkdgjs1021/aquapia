06bd9912d5d5f897ec0f8d99a9e1eeb208743742
tkdgjs1021 <75357075+tkdgjs1021@users.noreply.github.com>
2021-01-25 16:27:55 +0900

    Delete animal.js

--- js/animal.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-        
-    $(".card").on('click',function(){
-        $(".cardRotate").addClass("backRotate").removeClass("cardRotate");
-        $(this).addClass("cardRotate").removeClass("backRotate");
-    });
-    
-});
